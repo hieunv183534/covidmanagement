263a2605fe67b90cd91cee8d79723fe253f9baf5
hieunv183534 <hieu.nv183534@sis.hust.edu.vn>
2021-12-24 22:19:04 +0700

    loadListPost

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ window.onload = () => {
     initEvent();
     console.log(listColums.User);
     loadTable(listColums.User, listDatas.User);
+    loadListPost(listDatas.Posts)
 }
 
 function initEvent() {
@@ -19,7 +20,7 @@ function initEvent() {
     });
 }
 
-function loadTable(columns, datas){
+function loadTable(columns, datas) {
     var thead = parseHTML('<thead></thead>');
     console.log(thead);
     columns.forEach(col => {
@@ -29,21 +30,21 @@ function loadTable(columns, datas){
     document.querySelector("table").append(thead);
 
     var tbody = parseHTML('<tbody></tbody>');
-    datas.forEach(item=>{
+    datas.forEach(item => {
         var tr = parseHTML(`<tr></tr>`);
-        columns.forEach(col=>{
+        columns.forEach(col => {
             var td = parseHTML(`<td style="width: ${col.width};"><p>${item[`${col.field}`]}</p></td>`);
             tr.append(td);
         });
         tbody.append(tr);
-        tr.addEventListener('dblclick', ()=>{
+        tr.addEventListener('dblclick', () => {
             tableRowOnDBClick(item.name);
         })
     });
     document.querySelector("table").append(tbody);
 }
 
-function tableRowOnDBClick(name){
+function tableRowOnDBClick(name) {
     console.log(name);
     alert(name);
 }
@@ -52,4 +53,28 @@ function parseHTML(html) {
     var t = document.createElement('template');
     t.innerHTML = html;
     return t.content.firstChild;
+}
+
+function loadListPost(posts) {
+    var postList = document.querySelector(".post-list");
+    posts.forEach(post => {
+        var postEle = parseHTML(`<div class="post">
+        <div class="post-header">
+            <div class="left">
+                <p class="poster">${post.poster.name}</p>
+                <p class="post-time">${post.time}</p>
+            </div>
+            <div class="right">
+                <i class="fas fa-map-marker-alt"></i>
+                <div class="post-position">${post.poster.unitName}</div>
+            </div>
+            
+        </div>
+        <div class="post-body">
+            <h2 class="post-title">${post.title}</h2>
+            <p class="post-content">${post.content}</p>
+        </div>
+    </div>`);
+        postList.append(postEle);
+    });
 }
--- js/model/dataList.js
+++ js/model/dataList.js
@@ -38,6 +38,43 @@ class listDatas {
         }
     ];
 
+    static Posts = [{
+        poster: {
+            name: "Nguyễn Văn Hiếu 1",
+            unitName: "Phường Đồng Tâm"
+        },
+        title: "Thông báo 1!",
+        content: "Laboriosam sunt senectus et, proin tempore primis soluta aliquid dis cupidatat corrupti habitasse sagittis debitis! Ac lorem quibusdam, nisi luctus tempore optio. Ex laoreet, dicta exercitationem tortor doloribus sagittis numquam sagittis dis sagittis aliquam nobis hac nonummy curae amet recusandae.",
+        time: "20h 20/11/2021"
+    },
+    {
+        poster: {
+            name: "Nguyễn Văn Hiếu2",
+            unitName: "Phường Đồng Râm"
+        },
+        title: "Thông báo 2!",
+        content: "Laboriosam sunt senectus et, proin tempore primis soluta aliquid dis cupidatat corrupti habitasse sagittis debitis! Ac lorem quibusdam, nisi luctus tempore optio. Ex laoreet, dicta exercitationem tortor doloribus sagittis numquam sagittis dis sagittis aliquam nobis hac nonummy curae amet recusandae.",
+        time: "20h 20/11/2021"
+    },
+    {
+        poster: {
+            name: "Nguyễn Văn Hiếu3",
+            unitName: "Phường Đồng Dâm"
+        },
+        title: "Thông báo 3!",
+        content: "Laboriosam sunt senectus et, proin tempore primis soluta aliquid dis cupidatat corrupti habitasse sagittis debitis! Ac lorem quibusdam, nisi luctus tempore optio. Ex laoreet, dicta exercitationem tortor doloribus sagittis numquam sagittis dis sagittis aliquam nobis hac nonummy curae amet recusandae.",
+        time: "20h 20/11/2021"
+    },
+    {
+        poster: {
+            name: "Nguyễn Văn Hiếu4",
+            unitName: "Phường Đồng Lâm"
+        },
+        title: "Thông báo 4!",
+        content: "Laboriosam sunt senectus et, proin tempore primis soluta aliquid dis cupidatat corrupti habitasse sagittis debitis! Ac lorem quibusdam, nisi luctus tempore optio. Ex laoreet, dicta exercitationem tortor doloribus sagittis numquam sagittis dis sagittis aliquam nobis hac nonummy curae amet recusandae.",
+        time: "20h 20/11/2021"
+    }];
+
 
     static Province = [{
         name: 'Hà Tĩnh',
