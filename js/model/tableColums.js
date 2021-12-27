4a793a5ce44574b9e5df88358b310e15fc641a80
hieunv183534 <hieu.nv183534@sis.hust.edu.vn>
2021-12-27 22:37:49 +0700

    hieunv update table unit

--- js/model/tableColums.js
+++ js/model/tableColums.js
@@ -29,4 +29,61 @@ class listColums {
         }
     ];
 
+    static Unit = [
+        {
+            title : "Tên đơn vị",
+            style : "text-align: left;",
+            width: "min-width: 250px;",
+            field: 'unitName'
+        },
+        {
+            title : "Mã đơn vị",
+            style : "text-align: left;",
+            width: "min-width: 150px;",
+            field: 'unitCode'
+        },
+        {
+            title : "Mức báo động",
+            style : "text-align: left;",
+            width: "min-width: 120px;",
+            field: 'warningLevel'
+        },
+        {
+            title : "Tổng số ca nhiễm",
+            style : "text-align: right;  background-color: #edd8bb33;",
+            width: "min-width: 250px;",
+            field: 'totalCases'
+        },
+        {
+            title : "Tổng số ca tử vong",
+            style : "text-align: right;  background-color: #f5b8b833;",
+            width: "min-width: 250px;",
+            field: 'totalDeaths'
+        },
+        {
+            title : "Tổng số ca phục hồi",
+            style : "text-align: right; background-color: #afebae33;",
+            width: "min-width: 250px;",
+            field: 'totalRecovereds'
+        },
+        {
+            title : "Số ca nhiễm mới",
+            style : "text-align: right;  background-color: #edd8bb33;",
+            width: "min-width: 250px;",
+            field: 'lastUpdateCases'
+        },
+        {
+            title : "Số ca tử vong mới",
+            style : "text-align: right;  background-color: #f5b8b833;",
+            width: "min-width: 250px;",
+            field: 'lastUpdateDeaths'
+        },
+        {
+            title : "Số ca phục hồi mới",
+            style : "text-align: right; background-color: #afebae33;",
+            width: "min-width: 250px;",
+            field: 'lastUpdateRecovereds'
+        }
+    ];
+
 }
