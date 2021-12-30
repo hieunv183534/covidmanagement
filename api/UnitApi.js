b258cc10af7706f0d6bdc4b8c2b68df4461dd36e
Hieunm54 <hieunm.542@gmail.com>
2021-12-30 17:57:51 +0700

    update api for units

--- api/UnitApi.js
+++ api/UnitApi.js
@@ -1,12 +1,36 @@
 class UnitApi extends BaseApi{
     constructor(){
-        this.apiController = "Unit"
+        this.apiController = "unit/"
     }
 
     /**
+     * Lay thong tin cua cac don vi con. VD truy van Ha Noi se lay duoc cac quan (khong lay xa)
+     * @param {
+     *  unit: unitcode cua don vi: VD: |1|1|
+     * } params 
      * 
-     * @param {*} body object unit
+     * 
+     * @param {
+     *  page: trang nao dang duoc truy van - phuc vu cho phan trang
+     *  total: tong so cac don vi muon duoc truy van ( mac dinh la 20 don vi)
+     * } query 
+     *
+     * @param {
+     *  keyword: tu de seach ten cac don vi
+     * } body
+     * 
      * @returns 
+     * +, Thành công
+     * status: 200 OK
+     * {
+     *  message: "fetching list of units successfully",
+     *  data: unitList,
+     * }
+     * +, Thất bại
+     * status: 403
+     * {
+     *  err
+     * }
      */
     getById(body){
         const options = {
@@ -15,11 +39,39 @@ class UnitApi extends BaseApi{
                 'Content-Type': 'application/json',
                 'Authorization': localStorage['token']
             },
-            body: JSON.stringify(body)
+            body: JSON.stringify(body),
         };
 
-        return fetch(this.baseUrl + `${this.apiController}/${id}`, options).then(res => {
+        return fetch(this.baseUrl + `${this.apiController}/unit-info/${unit}`, options).then(res => {
             return res.json()
         });
     }
+
+    /**
+     * Cap nhat thong tin cac don vi con. Cac don vi cha, ong se duoc tu dong cap nhap
+     * @param {
+     * lastUpdateCases: so ca mac moi
+     * lastUpdateDeaths: so ca tu vong moi
+     * lastUpdateRecovereds: so ca phuc hoi moi
+     * } body
+     * 
+     * @param {
+     * unit: unitCode cua don vi can cap nhap
+     * } params
+     * 
+     * @returns 
+     * +, Thành công
+     * status: 200
+     * {
+     *  message: "Update successful",
+     * }
+     * 
+     * +, Thất bại
+     * status; 500
+     * {
+     *  err
+     * }
+     */
+
+
 }
